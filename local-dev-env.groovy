359df92f40042f4622499ff0ff35182417b1f44c
Chris Richard <christopher.richard27@outlook.com>
2018-01-21 19:01:25 -0800

    added job to build local dev environment with vagrant

---
+++ local-dev-env.groovy
@@ -0,0 +1,12 @@
+job('Build Dev Env') {
+    description('Build Local Development Environment with Vagrant and Virtualbox')
+    scm {
+        github('cbrichard/local-dev-env', 'master')
+    }
+    steps {
+        shell('echo Testing Vagrantfile')
+        shell('vagrant validate')
+        shell('echo Running Vagrant to build Dev Environment')
+        shell('vagrant up')
+    }
+}
